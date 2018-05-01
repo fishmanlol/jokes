d9b0125090c73f3028af2fcf1431826c86a37f00
tongyi <ty@ty.local>
2018-05-01 11:28:04 -0700

    Initial Commit

---
+++ jokes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jokes
+//
+//  Created by tongyi on 2018/5/1.
+//  Copyright © 2018年 tongyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
